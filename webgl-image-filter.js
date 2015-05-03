a8d3ea43d00091481fa8cd12b38c24bc00d600fb
nomelif <theo.friberg@gmail.com>
2015-05-03 11:06:52 +0300

    Added vertical sobel kernel

--- webgl-image-filter.js
+++ webgl-image-filter.js
@@ -516,6 +516,13 @@ var WebGLImageFilter = window.WebGLImageFilter = function() {
                 ]);
         };
 
+        _filter.sobelY = function() {
+                _filter.convolution.call(this, [
+                        -1, -2, -1,
+                         0,  0,  0,
+                         1,  2,  1
+                ]);
+        };
 
 	_filter.sharpen = function( amount ) {
 		var a = amount || 1;
